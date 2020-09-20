3b3eccfadf59a0f1a70defffb677867c615849a8
alee8599 <alee8599@stanford.edu>
2020-09-20 09:11:06 -0700

    changed app top bar color

--- test/bottom_navigation_widget_test.dart
+++ test/bottom_navigation_widget_test.dart
@@ -5,17 +5,24 @@ void main() {
   // a group of widget tests for the App bar and bottom navigation bar
   group('App Bar', () {
     testWidgets('validate top bar widget text', (WidgetTester tester) async {
-
       await tester.pumpWidget(MaterialApp(
         home: Scaffold(
           body: Text('U-Report'),
         ),
       ));
 
-
       expect(find.text('U-Report'), findsOneWidget);
     });
 
+    testWidgets('validate color for app bar', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.grey[50],
+        ),
+      ));
 
+      expect((tester.widget(find.byType(Material)) as Material).color,
+          Colors.grey[50]);
+    });
   });
 }
