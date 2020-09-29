75ef6855ac8c2f2c21dc322746570cb81f2de342
nathanbaleeta <nbaleeta@gmail.com>
2020-09-29 12:17:22 +0300

    MOdified directory structure to map features to independent directories including tests

--- test/stories/story_model_test.dart
+++ test/stories/story_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ureport_app/model/Story.dart';
+import 'package:ureport_app/stories/Story.dart';
+
 
 void main() {
   test('validate story model test data', () async {
