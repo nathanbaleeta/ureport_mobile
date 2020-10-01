f10eef033f9d0d530666490c9cb539544a53a607
nathanbaleeta <nbaleeta@gmail.com>
2020-10-01 13:58:38 +0300

    Moved test for StoryConverter.dart to separate file: story_converter_test.dart

--- test/stories/story_model_test.dart
+++ test/stories/story_model_test.dart
@@ -1,20 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ureport_app/stories/Story.dart';
-import '../../lib/stories/StoryConverter.dart';
 
 void main() {
-  test('validate story model test data', () async {
-    verifyTestData();
+  test('validate story model test data', () {
+
   });
-  
-  test("should return a list of Story", () async {
-    final storyConverter = StoryConverter();
-    final result = await storyConverter.getStories();
-    expect(result, isA<List<Story>>());
-  });
+
   
 }
 
-void verifyTestData() {
-
-}
