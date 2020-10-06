a4ca4c98707185e0f6ba40ea7b3f989cd0974b2f
ericcbear <ericchristopherbear@gmail.com>
2020-10-06 17:20:38 -0600

    Custom Story Model Dart Class Tests
    A suite of tests to evaluate whether the custom story model data conform to the predetermined test data provided to the converter. Specifically, this confirms the id, title, story count, and picture source for each the custom story class.

--- test/stories/story_model_test.dart
+++ test/stories/story_model_test.dart
@@ -1,10 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ureport_app/stories/Story.dart';
+import '../../lib/stories/StoryConverter.dart';
 
 void main() {
-  test('validate story model test data', () {
+  // a suite of tests to evaluate whether the custom story model data conform
+  // to the predetermined test data provided to the converter
+  test('Validate story model test data', () async {
+    final storyConverter = StoryConverter();
+    final stories = await storyConverter.getStories();
+    final idsToVerify = [713, 685, 677, 668, 648, 635, 625, 580, 579, 578];
+    expect(stories.length, 10); // verify the length of story test data
 
+    for (int i = 0; i < stories.length; i++) {
+      expect(stories[i].id, idsToVerify[i]); // verify hardcoded story data ids
+      // verify picture website source for each test story
+      expect(stories[i].picture.substring(0, 47),
+        "https://dl-ureport.s3.amazonaws.com/categories/");
+    }
+
+    // verify titles of story test data
+    expect(stories[6].title, "Virtual Reality in the Classroom");
+    expect(stories[9].title, "COMMON ERRORS JOB APPLICANTS MAKE");
   });
-
   
 }
+
 
