e96207a7139456d43cff6e09a1dee0b163e3150e
Karen Zhu <zhukarenm@gmail.com>
2020-10-07 15:47:29 -0400

    fixed old testing file

--- test/navigation/bottom_navigation_widget_test.dart
+++ test/navigation/bottom_navigation_widget_test.dart
@@ -10,25 +10,6 @@ Finder findBottomNavigationIcon(IconData icon) {
 void main() {
   // a group of widget tests for the App bar and bottom navigation bar
   group('App Bar', () {
-    testWidgets('validate top bar widget text', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-          body: Text('U-Report'),
-        ),
-      ));
-      expect(find.text('U-Report'), findsOneWidget);
-    });
-
-    testWidgets('validate color for app bar', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.grey[50],
-        ),
-      ));
-
-      expect((tester.widget(find.byType(Material)) as Material).color,
-          Colors.grey[50]);
-    });
 
     testWidgets(
         'Bottom Navigation Bar icons and text validation with tab-pressing validation',
