6b25b3124531f61b0912339a2be38d537c3907d9
Joseph Zhang <josephhzhangg@gmail.com>
2020-10-20 20:23:45 -0400

    commit before apptheme

--- test/stories/story_list_view_test.dart
+++ test/stories/story_list_view_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ureport_app/main.dart' as ureport_app;
+import 'package:ureport_app/stories/StoryListView.dart';
 
 /* TODO:
  * - Find entire storyListView
@@ -8,18 +9,24 @@ import 'package:ureport_app/main.dart' as ureport_app;
  * - Find dropdown menu
  */
 
+
 Finder findBottomNavigationIcon(IconData icon) {
   return find.descendant(
       of: find.byType(BottomNavigationBar), matching: find.byIcon(icon));
 }
 
-/* abstract class mockStoryListView implements StoryListView {
+/*
+abstract class mockStoryListView implements StoryListView {
   @override
   _StoryListViewState createState() => null;
-} */
+}
+*/
+
 
 void main() {
-  group('Story List View', () {
+  group('Story List View', ()
+  {
+
     testWidgets('validate list view appears', (WidgetTester tester) async {
       ureport_app.main();
       await tester.pump();
@@ -29,10 +36,22 @@ void main() {
 
       /*
       mockedStoryListView = mockStoryListView();
-      await tester.pump(mockStoryListView());
+      StoryListView storyListView = StoryListView()
+      await tester.pump(storyListView());
       var findStoryListView = find.byType(ListView);
       expect(findStoryListView, findsOneWidget);
-      */
+
+
+       */
+
+
     });
+
   });
+
+
 }
+
+
+
+
--- test/stories/story_model_test.dart
+++ test/stories/story_model_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ureport_app/stories/Story.dart';
 
 
+/*
 void main() {
   test('validate story model test data', () async {
     verifyTestData();
@@ -11,3 +12,5 @@ void main() {
 void verifyTestData() {
 
 }
+
+ */
