6c881a4543885c1766de59af4ecfb1efec83882d
clarakeng <44857435+clarakeng@users.noreply.github.com>
2020-11-02 11:54:42 +0800

    Editing for story_model_test.dart merge conflict

--- test/stories/story_model_test.dart
+++ test/stories/story_model_test.dart
@@ -1,9 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('validate story model test data', () async {
-    verifyTestData();
-  });
-}
-
-void verifyTestData() {}
+void main() {}
