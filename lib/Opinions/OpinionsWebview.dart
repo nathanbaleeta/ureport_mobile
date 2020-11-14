750c292f271c1e39f20be1fede4a2e8fc539d20f
Joseph Zhang <josephhzhangg@gmail.com>
2020-11-14 13:04:35 -0500

    Modified opinions to attempt to remove facebook messenger

--- lib/Opinions/OpinionsWebview.dart
+++ lib/Opinions/OpinionsWebview.dart
@@ -1,5 +1,8 @@
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+
 
 class OpinionsWebView extends StatefulWidget {
   @override
@@ -8,6 +11,8 @@ class OpinionsWebView extends StatefulWidget {
 
 class _OpinionsWebViewState extends State<OpinionsWebView> {
 
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+  WebViewController _myController;
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +21,33 @@ class _OpinionsWebViewState extends State<OpinionsWebView> {
       body: WebView(
           initialUrl: 'https://ureport.in/opinions/',
           javascriptMode: JavascriptMode.unrestricted,
-          //onWebViewCreated: (WebViewController WebViewController) {
-           // _controller.complete(WebViewController);
-          //},
+
+        navigationDelegate: (NavigationRequest request) {
+
+
+          if (request.url.contains('facebook.com/')) {
+            print('blocking navigation to request');
+            return NavigationDecision.prevent;
+          }
+          if (request.url.contains('messenger')) {
+            print('blocking navigation to request');
+            return NavigationDecision.prevent;
+          }
+
+          print('allowing navigation to $request');
+          return NavigationDecision.navigate;
+        },
+
+
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+            _myController = webViewController;
+          },
+
+        onPageFinished: (String url) {
+          print('Page finished loading: $url');
+          _myController.evaluateJavascript("document.getElementById(\"facebook\").style.display='none';");
+        },
         )
     );
   }
